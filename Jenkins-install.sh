fa5f8749a453e7cb8bda631e99c6e002b6704227
ngoparaju <n.goparaju@gmail.com>
2023-06-10 19:16:48 +0100

    Update Jenkins-install.sh

--- Jenkins-install.sh
+++ Jenkins-install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install openjdk-11-jre
+sudo apt install openjdk-11-jre -y
 java -version
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc >/dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
@@ -7,4 +7,3 @@ sudo apt-get update
 sudo apt-get install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-sudo systemctl status jenkins
